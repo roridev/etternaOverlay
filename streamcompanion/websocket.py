3baaaace29ca35f07b79fdb0e37f6d5ab86cc1c1
roridev <t3ctotalmenterandom1@outlook.com>
2021-07-25 21:11:23 -0300

    Port websocket module to trio

--- streamcompanion/websocket.py
+++ streamcompanion/websocket.py
@@ -1,8 +1,6 @@
-
-import websockets
 import json
-from asyncio_channel._channel import Channel
-
+from trio.abc import SendChannel
+from trio_websocket import open_websocket_url
 
 uri = "ws://localhost:20727/tokens"
 state = 0
@@ -13,26 +11,24 @@ retries = 0
 status = ""
 
 
-async def send_diff(hit_channel: Channel, new_state: list[int]):
+async def send_diff(hit_channel: SendChannel, new_state: list[int]):
     global state
     delta = new_state[state:]
     state = len(new_state)
 
     for item in delta:
-        if await hit_channel.put(item):
-            print(f'[hit_channel] -> Sent {item}.')
-        else:
-            print(f'[hit_channel] -\\> Blocked {item}.')
+        await hit_channel.send(item)
+        print(f'[hit_channel] -> Sent {item}.')
 
 
-async def connect(hit_channel: Channel, miss_channel: Channel, status_channel: Channel):
+async def connect(hit_channel: SendChannel, miss_channel: SendChannel, status_channel: SendChannel):
     global state
-    async with websockets.connect(uri) as ws:
+    async with open_websocket_url(uri) as ws:
         while True:
-            await ws.send("[\"hitErrors\",\"miss\","
-                          # Detects status change. Goes into status channel.
-                          "\"plays\",\"retries\",\"rawStatus\"]")
-            _recv = await ws.recv()
+            await ws.send_message("[\"hitErrors\",\"miss\","
+                                  # Detects status change. Goes into status channel.
+                                  "\"plays\",\"retries\",\"rawStatus\"]")
+            _recv = await ws.get_message()
             j = json.loads(_recv)
             if "hitErrors" in j:
                 if len(j["hitErrors"]) != state:
@@ -43,14 +39,14 @@ async def connect(hit_channel: Channel, miss_channel: Channel, status_channel: C
                     plays = j["plays"]
                     print(f'Detected new play. Resetting state')
                     state = 0
-                    await status_channel.put('p')
+                    await status_channel.send('p')
             if "retries" in j:
                 global retries
                 if j["retries"] != retries:
                     retries = j["retries"]
                     print(f'Detected retry. Resetting state')
                     state = 0
-                    await status_channel.put('r')
+                    await status_channel.send('r')
             if "rawStatus" in j:
                 global status
                 if j["rawStatus"] != status:
@@ -58,12 +54,10 @@ async def connect(hit_channel: Channel, miss_channel: Channel, status_channel: C
                         print(f'Detected status change. Resetting state')
                         state = 0
                     status = j["rawStatus"]
-                    await status_channel.put(status)
+                    await status_channel.send(status)
             global misses
             if "miss" in j:
                 if misses != j["miss"]:
                     misses = j["miss"]
-                    if await miss_channel.put(misses):
-                        print(f'[miss_channel] -> Sent {misses}.')
-                    else:
-                        print(f'[miss_channel] -\\> Blocked {misses}.')
+                    await miss_channel.send(misses)
+                    print(f'[miss_channel] -> Sent {misses}.')
